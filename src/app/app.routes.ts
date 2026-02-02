614b74ddc6f995cf8b467af401b981f5dbf0bcac
Alvitaa <juanjoalva19@gmail.com>
2026-02-01 21:04:17 -0500

    router functional

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,17 @@
 import { Routes } from '@angular/router';
 import { authGuard } from './core/auth/auth.guard';
+import { Login } from './features/auth/login/login';
+import { Dashboard } from './features/dashboard/dashboard';
 
 export const routes: Routes = [
   {
     path: 'login',
-    loadComponent: () =>
-      import('./features/auth/login/login').then((m) => m.Login),
+    component: Login,
   },
   {
     path: '',
     canActivate: [authGuard],
-    loadComponent: () => import('./features/dashboard/dashboard').then((m) => m.Dashboard),
+    component: Dashboard,
   },
   { path: '**', redirectTo: '' },
 ];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { Dashboard } from './features/dashboard/dashboard';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [Dashboard],
-  template: '<app-dashboard />',
+  imports: [RouterOutlet],
+  templateUrl: './app.html'
 })
 export class App {
   protected readonly title = signal('transport-data-dashboard');
