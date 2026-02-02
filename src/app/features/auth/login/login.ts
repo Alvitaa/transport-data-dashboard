05bd7b59ff4b44b9945bad76a46021c6a4a38bf1
Alvitaa <juanjoalva19@gmail.com>
2026-02-01 21:30:04 -0500

    login functional

--- src/app/features/auth/login/login.ts
+++ src/app/features/auth/login/login.ts
@@ -1,11 +1,36 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../../core/auth/auth.service';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
+  host: {
+    class: 'flex flex-1 w-full',
+  },
 })
 export class Login {
+  email = '';
+  password = '';
+  error = '';
 
+  constructor(
+    private auth: AuthService,
+    private router: Router,
+  ) {}
+
+  submit() {
+    const success = this.auth.login(this.email, this.password);
+
+    if (!success) {
+      this.error = 'Invalid credentials';
+      return;
+    }
+
+    this.router.navigate(['/']);
+  }
 }
